94b230be207f2f0e2f9188fc91ce59942f1201cd
Ben Mordue <benmordue@gmail.com>
2014-12-17 15:35:38 +0000

    Be sensible about env vars

--- constants.py
+++ constants.py
@@ -1,8 +1,12 @@
 __author__ = 'ben'
 
+import os
+
 NB_ENDPOINT = 'https://newsblur.com'
 NB_HN_FEED_ID = 6
-NB_CREDENTIALS = {'username': 'bmordue', 'password': 'tester'}
 DATABASE_NAME = 'stories.sqlite'
 MAX_PARSE = 50 # max number of Newsblur starred stories to process
 BATCH_SIZE = 10 # how many batches to parse and enter in DB in one go
+NB_USERNAME = os.getenv('NB_USERNAME','No username')
+NB_PASSWORD = os.getenv('NB_PASSWORD','No password')
+NB_CREDENTIALS = {'username': NB_USERNAME, 'password': NB_PASSWORD}
