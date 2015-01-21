062335e449a88d0c5b18fa2fee2872ac220e9988
Ben Mordue <benmordue@gmail.com>
2015-01-21 18:38:43 +0000

    Add backoff strategy

--- constants.py
+++ constants.py
@@ -4,10 +4,12 @@ import os
 
 NB_ENDPOINT = 'https://newsblur.com'
 NB_HN_FEED_ID = 6
-DATABASE_NAME = 'stories.sqlite'
-MAX_PARSE = 50 # max number of Newsblur starred stories to process
+DATABASE_FILENAME = 'stories.sqlite'
+MAX_PARSE = 5000 # max number of Newsblur starred stories to process
 BATCH_SIZE = 10 # how many batches to parse and enter in DB in one go
 NB_USERNAME = os.getenv('NB_USERNAME','No username')
 NB_PASSWORD = os.getenv('NB_PASSWORD','No password')
 NB_CREDENTIALS = {'username': NB_USERNAME, 'password': NB_PASSWORD}
 COMMENTS_THRESHOLD = 20
+VERIFY = True # useful to set to False to use with Charles proxy, but this generates loadsa warnings
+MAX_BACKOFF = 24*60*60  # in seconds
--- nb.py
+++ nb.py
@@ -2,23 +2,26 @@ __author__ = 'bmordue'
 
 import constants
 import requests
+import requests.exceptions
 import json
 import sqlite3
 from bs4 import BeautifulSoup
+from time import sleep
 
 
 def populate():
     print 'Set up DB and add a row for each HN story'
-    conn = sqlite3.connect(constants.DATABASE_NAME)
+    conn = sqlite3.connect(constants.DATABASE_FILENAME)
     c = conn.cursor()
     c.execute('''DROP TABLE IF EXISTS stories''')
     c.execute('''CREATE TABLE IF NOT EXISTS stories
              (hash TEXT UNIQUE, hnurl TEXT, url TEXT, added TEXT, comments INTEGER)''')
 
-    r = requests.post(constants.NB_ENDPOINT + '/api/login', constants.NB_CREDENTIALS, verify=False)
+    r = requests.post(constants.NB_ENDPOINT + '/api/login', constants.NB_CREDENTIALS, verify=constants.VERIFY)
     mycookies = r.cookies
 
-    hashes = requests.get(constants.NB_ENDPOINT + '/reader/starred_story_hashes', cookies=mycookies, verify=False)
+    hashes = requests.get(constants.NB_ENDPOINT + '/reader/starred_story_hashes',
+                          cookies=mycookies, verify=constants.VERIFY)
 
     hashlist = hashes.json()['starred_story_hashes']
 
@@ -50,7 +53,7 @@ def process_batch(cookie_store, cursor, batch):
 
     for a_hash in batch:
         req_str += 'h=' + a_hash + '&'
-    stories = requests.get(req_str, cookies=cookie_store, verify=False)
+    stories = requests.get(req_str, cookies=cookie_store, verify=constants.VERIFY)
     storylist = json.loads(stories.text)['stories']
 
     for story in storylist:
@@ -63,7 +66,7 @@ def process_batch(cookie_store, cursor, batch):
 # read through DB for rows without comment count, then add it
 def add_comment_counts():
     print 'Add comment counts to stories in DB'
-    conn = sqlite3.connect(constants.DATABASE_NAME)
+    conn = sqlite3.connect(constants.DATABASE_FILENAME)
     cursor = conn.cursor()
 
     cursor.execute("SELECT hnurl FROM stories WHERE comments IS NULL")
@@ -92,28 +95,42 @@ def parse_story(content):
 def get_comment_count(hnurl):
     comment_count = 0
     try:
-        story = requests.get(hnurl, verify=False)
-        if story.status_code == 200:
-            comment_count = parse_story(story.text)
-        else:
-            print "Request for %s returned %s response" % (hnurl, story.status_code)
-            return None
+        backoff = 5
+        story = requests.get(hnurl, verify=constants.VERIFY)
+        while (story.status_code != 200):
+            if story.status_code in [403, 500, 503]:  # exponential backoff
+                print "Request for %s returned %s response" % (hnurl, story.status_code)
+                if backoff < constants.MAX_BACKOFF:
+                    print "Backing off %s seconds" % backoff
+                    sleep(backoff)
+                    backoff = backoff*2
+                else:
+                    print "Giving up after %s seconds for %s" % (backoff, hnurl)
+                    return None
+            elif story.status_code == 520:
+                print "520 response, skipping %" % hnurl
+                return None
+            else:
+                print "Request for %s returned unhandled %s response" % (hnurl, story.status_code)
+                raise requests.exceptions.RequestException()
     except requests.exceptions.RequestException as e:
         print "hnurl: " + hnurl
         print e
+        return None
+    comment_count = parse_story(story.text)
     return comment_count
 
 
 def remove_star(story_hash):
-    r = requests.post(constants.NB_ENDPOINT + '/api/login', constants.NB_CREDENTIALS, verify=False)
+    r = requests.post(constants.NB_ENDPOINT + '/api/login', constants.NB_CREDENTIALS, verify=constants.VERIFY)
     mycookies = r.cookies
     requests.post(constants.NB_ENDPOINT + '/reader/mark_story_hash_as_unstarred',
-                  {'story_hash': story_hash}, cookies=mycookies, verify=False)
+                  {'story_hash': story_hash}, cookies=mycookies, verify=constants.VERIFY)
 
 
 def prune_starred():
     print 'Remove all stars on stories with less than %i comments' % constants.COMMENTS_THRESHOLD
-    conn = sqlite3.connect(constants.DATABASE_NAME)
+    conn = sqlite3.connect(constants.DATABASE_FILENAME)
     cursor = conn.cursor()
 
     cursor.execute("SELECT hash FROM stories WHERE (comments < ?)", (constants.COMMENTS_THRESHOLD,))
@@ -129,9 +146,9 @@ def prune_starred():
 
 
 def check_if_starred(story_hash):
-    r = requests.post(constants.NB_ENDPOINT + '/api/login', constants.NB_CREDENTIALS, verify=False)
+    r = requests.post(constants.NB_ENDPOINT + '/api/login', constants.NB_CREDENTIALS, verify=constants.VERIFY)
     mycookies = r.cookies
-    hashes = requests.get(constants.NB_ENDPOINT + '/reader/starred_story_hashes', cookies=mycookies, verify=False)
+    hashes = requests.get(constants.NB_ENDPOINT + '/reader/starred_story_hashes', cookies=mycookies, verify=constants.VERIFY)
     hashlist = hashes.json()['starred_story_hashes']
 
     if story_hash in hashlist:
@@ -142,10 +159,11 @@ def check_if_starred(story_hash):
 
 if __name__ == "__main__":
     print "__main__"
-    import sys
+    import sys, os
     if sys.argv[0]:
         constants.MAX_PARSE = sys.argv[0]
-#    populate()
+    if not os.path.isfile(constants.DATABASE_FILENAME):
+        populate()
     add_comment_counts()
 #    prune_starred()
     print 'Done.'
