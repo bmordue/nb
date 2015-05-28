5bd5827de01a0c53406e2b3dc2cece5961e6b4da
Ben Mordue <benmordue@gmail.com>
2015-05-28 15:05:18 +0000

    Revert retry on 520

--- populate.py
+++ populate.py
@@ -167,8 +167,9 @@ def get_comment_count(hnurl):
                     logger.debug("Giving up after {0} seconds for {1}".format(backoff, hnurl))
                     return None
             elif story.status_code == 520:
-                logger.debug("520 response for {0} and waiting {1} sec".format(hnurl, constants.BACKOFF_ON_520))
+                logger.debug("520 response for {0}; waiting {1} sec".format(hnurl, constants.BACKOFF_ON_520))
                 sleep(constants.BACKOFF_ON_520)
+                return None
             else:
                 logger.debug("Request for {0} returned unhandled {1} response".format(hnurl, story.status_code))
                 raise requests.exceptions.RequestException()
