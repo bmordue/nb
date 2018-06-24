af6ff8aff853cc3690915c4df20443170f140d73
Ben Mordue <benmordue@gmail.com>
2018-06-24 19:33:32 +0100

    Update nb.py

--- nb.py
+++ nb.py
@@ -6,7 +6,10 @@ from tasks.add_comment_counts import add_comment_counts
 from tasks.prune import prune_starred
 from tasks.add_domains import add_domains
 from utility import nb_logging
+import rollbar
 
+rollbar.init('00b402fc0da54ed1af8687d4c4389911')
+rollbar.report_message('Rollbar is configured correctly')
 logger = nb_logging.setup_logger('NB')
 
 if __name__ == "__main__":
