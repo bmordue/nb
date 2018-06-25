a75165fd48e9d13b9c9dcc937e1afc541ab57695
ben <benmordue@gmail.com>
2018-06-25 18:48:15 +0000

    nb_exceptions

--- connectors/NewsblurConnector.py
+++ connectors/NewsblurConnector.py
@@ -7,7 +7,7 @@ import requests.exceptions
 from datadog import statsd
 from bs4 import BeautifulSoup
 
-from exceptions import *
+from nb_exceptions import BlacklistedError
 from utility import constants
 from utility import nb_logging
 
