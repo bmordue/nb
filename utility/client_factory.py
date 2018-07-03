fa8bdd0c6ccbdb27980c89f511ea059d6fb3eeb5
Ben Mordue <benmordue@gmail.com>
2018-07-03 21:13:31 +0100

    Fix some bloopers

--- utility/client_factory.py
+++ utility/client_factory.py
@@ -1,5 +1,7 @@
 from connectors.MySqlClient import MySqlClient
-import utility.constants
+import utility.constants as constants
+
 
 def get_db_client():
-    return MySqlClient(host=constants.DB_HOST,user=constants.DB_USER,password=constants.DB_PASS,db_name=constants.DB_NAME)
+    return MySqlClient(host=constants.DB_HOST, user=constants.DB_USER, password=constants.DB_PASS,
+                       db_name=constants.DB_NAME)
--- utility/constants.py
+++ utility/constants.py
@@ -5,10 +5,10 @@ import os
 NB_ENDPOINT = 'https://newsblur.com'
 NB_HN_FEED_ID = 5982259
 #DATABASE_FILENAME = 'stories.sqlite'
-DB_HOST = os.getenv('DB_HOST','93.170.105.134')
+DB_HOST = os.getenv('DB_HOST','stage.benmordue.co.uk')
 # DB_PORT = 5432
-DB_USER = os.getenv('DB_USER','nbuser')
-DB_PASS = os.getenv('DB_PASS','nbpass')
+DB_USER = os.getenv('DB_USER','No username')
+DB_PASS = os.getenv('DB_PASS','No password')
 DB_NAME = 'nb_stories'
 MAX_PARSE = 9000 # max number of Newsblur starred stories to process
 BATCH_SIZE = 10 # how many batches to parse and enter in DB in one go
