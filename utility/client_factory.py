c65a35a3f1d63908ff86745dba65872e081d0e2a
Ben Mordue <benmordue@gmail.com>
2018-07-03 19:59:07 +0000

    remove relative import

--- utility/client_factory.py
+++ utility/client_factory.py
@@ -1,5 +1,5 @@
 from connectors.MySqlClient import MySqlClient
-import constants
+import utility.constants
 
 def get_db_client():
     return MySqlClient(host=constants.DB_HOST,user=constants.DB_USER,password=constants.DB_PASS,db_name=constants.DB_NAME)
