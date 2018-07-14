1437e490589c6d2c9ae558d3ced43101ebcbd87b
Ben Mordue <benmordue@gmail.com>
2018-07-14 15:08:13 +0100

    Codefactor warning
    Avoid comparison to string literal

--- nb.py
+++ nb.py
@@ -25,12 +25,12 @@ if __name__ == "__main__":
     statsd.event("Starting nb", "Parse up to {0} stories in batches of {1}"
                  .format(config.get('MAX_PARSE'), config.get('BATCH_SIZE')))
 
-    if config.get('SHOULD_POPULATE') is 'True':
+    if distutils.util.strtobool(config.get('SHOULD_POPULATE')):
         populate()
-    if config.get('SHOULD_ADD_DOMAINS') is 'True':
+    if distutils.util.strtobool(config.get('SHOULD_ADD_DOMAINS')):
         add_domains()
-    if config.get('SHOULD_ADD_COMMENT_COUNTS') is 'True':
+    if distutils.util.strtobool(config.get('SHOULD_ADD_COMMENT_COUNTS')):
         add_comment_counts()
-    if config.get('SHOULD_PRUNE_STARRED') is 'True':
+    if distutils.util.strtoboolconfig.get('SHOULD_PRUNE_STARRED')):
         prune_starred()
     logger.info('Done.')
