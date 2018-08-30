066094efa8dcde62f98147cdb12457d4a5b3c29f
Ben Mordue <benmordue@gmail.com>
2018-08-30 21:25:05 +0000

    dockerising

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
- docker run --rm \
-	-e NB_USERNAME=$NB_USERNAME6 \
+source .env
+
+sudo docker run --rm \
+	-e NB_USERNAME=$NB_USERNAME \
 	-e NB_PASSWORD=$NB_PASSWORD \
 	-e DB_HOST=$DB_HOST \
 	-e DB_USER=$DB_USER \
 	-e DB_PASS=$DB_PASS \
 	bmordue/nb
-
--- src/utility/client_factory.py
+++ src/utility/client_factory.py
@@ -1,7 +1,14 @@
 from connectors.MySqlClient import MySqlClient
 import os
+from utility import nb_logging
+
+logger = nb_logging.setup_logger('client_factor')
 
 
 def get_db_client():
-    return MySqlClient(host=os.getenv('DB_HOST'), user=os.getenv('DB_USER'), password=os.getenv('DB_PASS'),
+    host = os.getenv('DB_HOST')
+    user = os.getenv('DB_USER')
+    password = os.getenv('DB_PASS')
+    logger.debug('host: {}'.format(host))
+    return MySqlClient(host=host, user=user, password=password,
                        db_name=os.getenv('DB_NAME', 'nb'))
