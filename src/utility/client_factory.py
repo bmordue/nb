15dd2cddad28d7551a54630fddfe95164d662468
Ben Mordue <bmordue@adobe.com>
2018-10-23 16:30:42 +0100

    Read Docker secrets in client factory

--- src/utility/client_factory.py
+++ src/utility/client_factory.py
@@ -1,14 +1,31 @@
 from connectors.MySqlClient import MySqlClient
-import os
+from connectors.NewsblurConnector import NewsblurConnector
 from utility import nb_logging
+import os
 
-logger = nb_logging.setup_logger('client_factor')
+logger = nb_logging.setup_logger('client_factory')
+SECRETS_DIR = os.getenv('SECRETS_DIR', '/run/secrets/')
+
+
+def get_secret(name):
+    with open(os.path.join(SECRETS_DIR, name), 'r') as f:
+        secret = f.read().strip()
+    return secret
 
 
 def get_db_client():
-    host = os.getenv('DB_HOST')
-    user = os.getenv('DB_USER')
-    password = os.getenv('DB_PASS')
+    host = get_secret('DB_HOST')
+    user = get_secret('DB_USER')
+    password = get_secret('DB_PASS')
+    db_name = get_secret('DB_NAME')
     logger.debug('host: {}'.format(host))
-    return MySqlClient(host=host, user=user, password=password,
-                       db_name=os.getenv('DB_NAME', 'nb'))
+    return MySqlClient(host=host, user=user, password=password, db_name=db_name)
+
+
+def get_newsblur_client():
+    db_client = get_db_client()
+
+    username = get_secret('NB_USERNAME')
+    password = get_secret('NB_PASSWORD')
+
+    return NewsblurConnector(db_client.read_config(), username, password)
