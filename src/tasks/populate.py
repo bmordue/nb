e3c5d00713d876005cc862c4046e7008edda76f4
Ben Mordue <bmordue@adobe.com>
2018-10-25 16:55:14 +0100

    Bits n pieces

--- src/tasks/populate.py
+++ src/tasks/populate.py
@@ -1,70 +1,60 @@
 from datadog import statsd
-
-import time
 from utility import client_factory
 from utility import nb_logging
 
-from connectors.NewsblurConnector import NewsblurConnector
-
 logger = nb_logging.setup_logger('populate')
-config = None
+
+
+@statsd.timed('nb.populate.update_hash_list')
+def update_hash_list():
+    logger.info('Get full list of NB story hashes')
+
+    nb_client = client_factory.get_newsblur_client()
+    nb_client.login()
+    hash_list = nb_client.get_nb_hash_list()
+    logger.info('Size of hashlist retrieved from Newsblur is %s', len(hash_list))
+    db_client = client_factory.get_db_client()
+    db_client.add_hashes(hash_list)
 
 
 @statsd.timed('nb.populate.populate')
 def populate():
-    logger.info('Set up DB and add a row for each HN story')
+    logger.info('Add a row for each HN story')
 
     db_client = client_factory.get_db_client()
     db_client.ensure_stories_table_exists()
     config = db_client.read_config()
     db_client.close_connection()
 
-    nb_client = NewsblurConnector(config)
+    nb_client = client_factory.get_newsblur_client()
     nb_client.login()
-    hashlist = nb_client.get_nb_hash_list()
-
-    logger.info('Size of hashlist is %s', len(hashlist))
 
     batch_size = int(config.get('BATCH_SIZE'))
     logger.debug('Batch size is %s', batch_size)
 
     i = 0
     count_batches = 0
-    batch = []
-    batchcounter = 0
-    for ahash in hashlist:
-        i += 1
-        if i >= int(config.get('MAX_PARSE')):
-            logger.info('Reached MAX_PARSE (%s)', config.get('MAX_PARSE'))
+    while i < int(config.get('MAX_PARSE')):
+        batch = db_client.read_hashes(batch_size)
+        if len(batch) == 0:
             break
-        if batchcounter >= batch_size:
-	    logger.debug('Process batch of %s', batch_size)
-            process_batch_with_retries(nb_client.get_story_list(batch), config)
-            count_batches += 1
-            batchcounter = 0
-            batch = []
-        batchcounter += 1
-        batch.append(ahash)
-    logger.debug('Process final batch of %s', len(batch))
-    process_batch(nb_client.get_story_list(batch), config)
-    count_batches += 1
+        count_batches += 1
+        i += len(batch)
+        logger.debug('Process batch of %s', len(batch))
+        process_batch_with_retries(nb_client.get_story_list(batch), config)
     logger.info('Finished adding story hashes to DB.')
     logger.info('Processed %s hashes in %s batches.', i, count_batches)
+
 
 @statsd.timed('nb.populate.process_batch_with_retries')
 def process_batch_with_retries(story_list, config):
     process_batch(story_list, config)
-#    for backoff in backoff_gen():
-#	try:
-#	    process_batch(story_list, config)
-#	    break
-#	except e:
-#	    logger.debug('Failed to process batch for %s; backing off % sec', story_list, backoff)
-#	    time.sleep(backoff)
+
 
 def backoff_gen():
     for i in range(0, 4):
-        yield 10*(i**2)
+        yield 10 * (i ** 2)
+
 
 # Process a batch of hashes and add details to DB
 @statsd.timed('nb.populate.process_batch')
@@ -76,8 +66,10 @@ def process_batch(story_list, config):
             db_client.add_story(story['story_hash'], story['story_date'], hnurl,
                                 story['story_permalink'])
             statsd.increment('nb.stories_added')
+        db_client.mark_story_done(story['story_hash'])
     statsd.increment('nb.stories.batches_processed')
     db_client.close_connection()
+
 
 def get_hn_url(content):
     return content.split('"')[1]
