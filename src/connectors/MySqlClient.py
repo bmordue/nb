2e4fcda85c0dfaf6ba7c619cbdc5fa811ef12023
Ben Mordue <benmordue@gmail.com>
2018-11-10 22:11:40 +0000

    wrapper for mysql query execution to add rollbar reporting

--- src/connectors/MySqlClient.py
+++ src/connectors/MySqlClient.py
@@ -8,6 +8,8 @@ from connectors.DbConnector import DbConnector
 from utility import nb_logging
 from utility.NbConfig import NbConfig
 
+import rollbar
+
 logger = nb_logging.setup_logger('MySqlClient')
 patch_all()
 STATSD_PREFIX = 'nb.MySqlClient.'
@@ -35,16 +37,14 @@ class MySqlClient(DbConnector):
                  toplevel_new VARCHAR(128), FOREIGN KEY (nb_hash) REFERENCES stories (hash) ) 
                  CHARACTER SET utf8'''
 
-        cursor = self.conn.cursor()
-        cursor.execute(create_table_query)
+        cursor = self.execute_wrapper(create_table_query)
         cursor.close()
         self.conn.commit()
         logger.info('Executed table creation query')
 
     @statsd.timed(STATSD_PREFIX + 'list_urls')
     def list_urls(self):
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT hash, url FROM stories")
+        cursor = self.execute_wrapper_wrapper("SELECT hash, url FROM stories")
         rows = cursor.fetchall()
         cursor.close()
         logger.info('Found %s results.', len(rows))
@@ -52,8 +52,7 @@ class MySqlClient(DbConnector):
 
     @statsd.timed(STATSD_PREFIX + 'insert_domain_entry')
     def insert_domain_entry(self, nb_hash, nb_url, domain, toplevel, toplevel_new):
-        cursor = self.conn.cursor()
-        cursor.execute(
+        cursor = self.execute_wrapper(
             '''INSERT IGNORE INTO domains (nb_hash, domain, toplevel, toplevel_new) VALUES 
             (%s, %s, %s, %s)''',
             (nb_hash, domain, toplevel, toplevel_new))
@@ -66,8 +65,7 @@ class MySqlClient(DbConnector):
 
     @statsd.timed(STATSD_PREFIX + 'list_stories_with_comments_fewer_than')
     def list_stories_with_comments_fewer_than(self, threshold):
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT hash FROM stories WHERE comments < %s AND starred = 1", (threshold,))
+        cursor = self.execute_wrapper_wrapper("SELECT hash FROM stories WHERE comments < %s AND starred = 1", (threshold,))
         rows = cursor.fetchall()
         cursor.close()
         logger.info('Found %s starred stories with fewer than %s comments', len(rows), threshold)
@@ -75,8 +73,7 @@ class MySqlClient(DbConnector):
 
     @statsd.timed(STATSD_PREFIX + 'unstar')
     def unstar(self, nb_hash):
-        cursor = self.conn.cursor()
-        cursor.execute("UPDATE stories SET starred = 0 WHERE hash = %s", (nb_hash,))
+        cursor = self.execute_wrapper("UPDATE stories SET starred = 0 WHERE hash = %s", (nb_hash,))
         cursor.close()
         self.conn.commit()
 
@@ -85,18 +82,16 @@ class MySqlClient(DbConnector):
              (hash VARCHAR(64) UNIQUE, hnurl TEXT, url TEXT, added TEXT, comments INTEGER,
              starred BOOLEAN DEFAULT 1) CHARACTER SET utf8'''
 
-        cursor = self.conn.cursor()
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            cursor.execute(table_setup_query)
+            cursor = self.execute_wrapper(table_setup_query)
         cursor.close()
         self.conn.commit()
 
     @statsd.timed(STATSD_PREFIX + 'add_story')
     def add_story(self, nb_hash, added, comments_url, story_url):
         insert_story_query = '''INSERT IGNORE INTO stories (hash, added, hnurl, url) VALUES (%s, %s, %s, %s)'''
-        cursor = self.conn.cursor()
-        cursor.execute(insert_story_query, (nb_hash, added, comments_url, story_url))
+        cursor = self.execute_wrapper(insert_story_query, (nb_hash, added, comments_url, story_url))
         cursor.close()
         self.conn.commit()
 	logger.info('Added story (%s)', nb_hash)
@@ -104,8 +99,7 @@ class MySqlClient(DbConnector):
     @statsd.timed(STATSD_PREFIX + 'list_stories_without_comment_count')
     def list_stories_without_comment_count(self):
         query = '''SELECT hnurl FROM stories WHERE comments IS NULL'''
-        cursor = self.conn.cursor()
-        cursor.execute(query)
+        cursor = self.execute_wrapper(query)
         rows = cursor.fetchall()
         cursor.close()
         return list(rows)
@@ -113,8 +107,7 @@ class MySqlClient(DbConnector):
     @statsd.timed(STATSD_PREFIX + 'add_comment_count')
     def add_comment_count(self, comments_url, count):
         query = '''UPDATE stories SET comments = %s WHERE hnurl = %s'''
-        cursor = self.conn.cursor()
-        cursor.execute(query, (count, comments_url))
+        cursor = self.execute_wrapper(query, (count, comments_url))
         cursor.close()
         self.conn.commit()
 	logger.info('Added comment count for %s (%s)', comments_url, count)
@@ -126,25 +119,30 @@ class MySqlClient(DbConnector):
         table_setup_query = '''CREATE TABLE IF NOT EXISTS config
             (config_key VARCHAR(64) UNIQUE, config_value TEXT)
              CHARACTER SET utf8'''
-        cursor = self.conn.cursor()
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            cursor.execute(table_setup_query)
+            cursor = self.execute_wrapper(table_setup_query)
         cursor.close()
         self.conn.commit()
 
     def read_config(self):
         query = '''SELECT * FROM config'''
-        cursor = self.conn.cursor()
-        cursor.execute(query)
+        cursor = self.execute_wrapper(query)
         cursor.close()
         rows = cursor.fetchall()
         return NbConfig(dict(rows))
 
     def write_config(self, config):
         query = '''REPLACE INTO config (config_key, config_value) VALUES (%s, %s)'''
-        cursor = self.conn.cursor()
         for key in config:
-            cursor.execute(query, (key, config[key]))
+            cursor = self.execute_wrapper(query, (key, config[key]))
         cursor.close()
         self.conn.commit()
+
+    def execute_wrapper(self, query_str, query_params):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute(query_str, query_params)
+        except:
+            rollbar.report_exc_info()
+        return cursor
